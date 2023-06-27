c17e48f5c0edc0c2baf16ebb8035a249f8a2732f
marelolemon <marelolemon@gmail.com>
2023-06-27 12:15:12 +0300

    add screenshot

--- order_tests.py
+++ order_tests.py
@@ -9,6 +9,7 @@ def post_new_order(body):
 def get_order_by_track_number(track):
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + "?t=" + str(track),
                         headers=data.headers)
+# Мария Клыкова, 1-я когорта — Финальный проект. Инженер по тестированию плюс
 def test_get_created_order():
     create_order_response = post_new_order(data.order_body)
     track_number = create_order_response.json()["track"]
